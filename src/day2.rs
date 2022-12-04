333fa8fbb2f4115078d180e352f8b69f50c0cac8
SteWil <32218463+Stewil@users.noreply.github.com>
2022-12-04 01:09:20 +0100

    finished day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -52,6 +52,36 @@ fn day2_part1(move_pairs: &[Vec<Move>]) -> i32 {
     score
 }
 
+#[aoc(day2, part2)]
+fn day2_part2(move_pairs: &[Vec<Move>]) -> i32 {
+    let mut iter = move_pairs.iter();
+    let mut score: i32 = 0;
+    while let Some(pair) = iter.next() {
+        match pair[0] {
+            // for sake of not redoing structures
+            // when looking at pair[1], its not moves
+            // rock:lose, paper:draw, scissors:win
+            // i should just ahve used the chars directly
+            Move::Rock => match pair[1] {
+                Move::Rock => score += 3 + 0,
+                Move::Paper => score += 1 + 3,
+                Move::Scissors => score += 2 + 6,
+            },
+            Move::Paper => match pair[1] {
+                Move::Rock => score += 1 + 0,
+                Move::Paper => score += 2 + 3,
+                Move::Scissors => score += 3 + 6,
+            },
+            Move::Scissors => match pair[1] {
+                Move::Rock => score += 2 + 0,
+                Move::Paper => score += 3 + 3,
+                Move::Scissors => score += 1 + 6,
+            },
+        }
+    }
+    score
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
